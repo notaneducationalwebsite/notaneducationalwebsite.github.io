db9cbe5b24e394b5951ef170d94e71b5d7b53775
notaneducationalwebsite <70805492+notaneducationalwebsite@users.noreply.github.com>
2020-09-13 14:28:33 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-function authenticateKey(){
-    var key = document.getElementById('key').value;
-    if(key == 'qazxswedcvfr'){
-        window.open("https://hss3uro2hsxfogfq.onion");
+function verifyService(){
+    var service = document.getElementById("serviceList").value;
+    if(service == 'null'){
+        alert("please select a proper service");
     }
-    else{
-        alert("Wrong Key");
+    else if(service == 'notevil'){
+        alert("Not Evil Selected - service will be added soon");
+    }
+    else if(service == 'gdark'){
+        alert("G Dark Selected - service will be added soon");
+    }
+    else if(service == 'hiddenwiki'){
+        alert("Hidden Wiki Selected - service will be added soon");
+    }
+    else if(service == 'hiddenanswers'){
+        alert("Hidden Answers Selected - service will be added soon");
+    }
+    else if(service == 'torrents'){
+        alert("Onion Lighning Fast Torrents - service will be added soon");
+    }
+    else if(service == 'bonus'){
+        alert("Contact Admin For More Info About Bonus");
     }
 }
